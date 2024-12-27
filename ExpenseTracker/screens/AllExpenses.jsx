7665c303266f233dfb481934f9e825766b8d4c3a
Dzhem Rushen Syuleyman <122823771+superGemHere@users.noreply.github.com>
2024-12-27 17:28:40 +0200

    feat: change to re-usable component

--- ExpenseTracker/screens/AllExpenses.jsx
+++ ExpenseTracker/screens/AllExpenses.jsx
@@ -1,11 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
+import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput'
+
 const AllExpenses = () => {
   return (
-    <View>
-      <Text>AllExpenses</Text>
-    </View>
+    <ExpensesOutput expensesPeriod={"Total"} />
   )
 }
 
