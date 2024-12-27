5bcd83952f2e97bd88b9e49d518b3fcb4bf9fe10
Dzhem Rushen Syuleyman <122823771+superGemHere@users.noreply.github.com>
2024-12-27 19:36:59 +0200

    Create Button.jsx

---
+++ ExpenseTracker/components/UI/Button.jsx
@@ -0,0 +1,57 @@
+import { View, Text, Pressable, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { GlobalStyles } from "../../constants/styles";
+
+const Button = ({ children, onPress, mode, style }) => {
+  const [isPressed, setPressed] = useState(false);
+  return (
+    <View style={style}>
+      <Pressable
+        onPress={onPress}
+        onPressIn={() =>setPressed(true)}
+        onPressOut={() => setPressed(false)}
+        style={({ pressed }) => pressed && styles.pressed}
+      >
+        <View
+          style={[
+            styles.button,
+            mode === "flat" && styles.flat,
+            
+          ]}
+        >
+          <Text style={[styles.buttonText, mode === "flat" && styles.flatText, isPressed && styles.pressedText,]}>
+            {children}
+          </Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+};
+
+export default Button;
+
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: 4,
+    padding: 8,
+    backgroundColor: GlobalStyles.colors.primary500,
+  },
+  flat: {
+    backgroundColor: "transparent",
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  flatText: {
+    color: GlobalStyles.colors.primary200,
+  },
+  pressed: {
+    opacity: 0.75,
+    backgroundColor: GlobalStyles.colors.primary100,
+    borderRadius: 4,
+  },
+  pressedText: {
+    color: "white",
+  },
+});
