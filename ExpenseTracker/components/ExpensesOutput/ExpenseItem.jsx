b34955f935d174f6d2e834b4f87df2d0e5282a6b
Dzhem Rushen Syuleyman <122823771+superGemHere@users.noreply.github.com>
2024-12-27 17:27:49 +0200

    Create ExpenseItem.jsx

---
+++ ExpenseTracker/components/ExpensesOutput/ExpenseItem.jsx
@@ -0,0 +1,79 @@
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import React from "react";
+import { GlobalStyles } from "../../constants/styles";
+import { getFormattedDate } from "../../utils/date";
+import { useNavigation } from "@react-navigation/native";
+
+const ExpenseItem = ({ description, amount, date }) => {
+  const navigation = useNavigation();
+
+  const expensePressHandler = () => {
+   navigation.navigate("ManageExpense")
+  };
+
+  return (
+    <Pressable
+      onPress={expensePressHandler}
+      style={({ pressed }) => pressed && styles.pressed}
+    >
+      <View style={styles.expenseItem}>
+        <View>
+          <Text style={[styles.textBase, styles.description]}>
+            {description}
+          </Text>
+          <Text style={styles.textBase}>
+            {getFormattedDate(date)}
+          </Text>
+        </View>
+        <View style={styles.amountContainer}>
+          <Text style={styles.amount}>
+            ${amount.toFixed(2)}
+          </Text>
+        </View>
+      </View>
+    </Pressable>
+  );
+};
+
+export default ExpenseItem;
+
+const styles = StyleSheet.create({
+  pressed: {
+    opacity: 0.5,
+  },
+  expenseItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 12,
+    marginVertical: 8,
+    borderRadius: 6,
+    elevation: 3,
+    shadowOffset: { width: 1, height: 1 },
+    shadowRadius: 4,
+    shadowOpacity: 0.4,
+    shadowColor: GlobalStyles.colors.gray500,
+    backgroundColor: GlobalStyles.colors.primary500,
+  },
+  textBase: {
+    color: GlobalStyles.colors.primary50,
+  },
+  description: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginBottom: 4,
+  },
+  amountContainer: {
+    paddingHorizontal: 12,
+    paddingVertical: 4,
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 4,
+  },
+  amount: {
+    color: GlobalStyles.colors.primary500,
+    fontWeight: "bold",
+    minWidth: 80,
+    textAlign: "center",
+  },
+});
