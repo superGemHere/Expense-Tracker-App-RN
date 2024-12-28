58c371aa353f24332624e0c25e70bc3ed420d102
Dzhem Rushen Syuleyman <122823771+superGemHere@users.noreply.github.com>
2024-12-28 14:21:00 +0200

    Create Input.jsx

---
+++ ExpenseTracker/components/ManageExpense/Input.jsx
@@ -0,0 +1,47 @@
+import { View, Text, TextInput, StyleSheet } from "react-native";
+import React from "react";
+import { GlobalStyles } from "../../constants/styles";
+
+const Input = ({ label, style, textInputConfig }) => {
+  let inputStyle = [styles.input];
+
+  if (textInputConfig && textInputConfig.multiline) {
+    inputStyle.push(styles.inputMultiline);
+  }
+
+  return (
+    <View style={[styles.inputContainer, style]}>
+      <Text style={styles.label}>
+        {label}
+      </Text>
+      <TextInput {...textInputConfig} style={inputStyle} />
+    </View>
+  );
+};
+
+export default Input;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginHorizontal: 4,
+    marginVertical: 8
+  },
+  label: {
+    fontSize: 12,
+    color: GlobalStyles.colors.primary100,
+    marginBottom: 4
+  },
+  input: {
+    padding: 6,
+    fontSize: 18,
+    borderRadius: 6,
+    color: GlobalStyles.colors.primary700,
+    backgroundColor: GlobalStyles.colors.primary100
+    //  borderColor: GlobalStyles.colors.primary50,
+    //  borderWidth: 1.1
+  },
+  inputMultiline: {
+    minHeight: 100,
+    textAlignVertical: "top"
+  }
+});
