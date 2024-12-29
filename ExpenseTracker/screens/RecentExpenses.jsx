4fb4f007238266f2fe9f5bc9629d810bd6c498f9
Dzhem Rushen Syuleyman <122823771+superGemHere@users.noreply.github.com>
2024-12-29 20:31:44 +0200

    feat: show loading indicator

--- ExpenseTracker/screens/RecentExpenses.jsx
+++ ExpenseTracker/screens/RecentExpenses.jsx
@@ -4,13 +4,17 @@ import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 import { ExpensesContext } from "../store/context/expenses-context";
 import { getDateMinusDays } from "../utils/date";
 import { getExpenses, } from "../utils/http";
+import LoadingOverlay from "../components/UI/LoadingOverlay";
 
 const RecentExpenses = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const { expenses, setExpenses } = useContext(ExpensesContext);
 
   useEffect(() => {
     async function resolveExpenses() {
+      setIsLoading(true);
       const expenses = await getExpenses();
+      setIsLoading(false);
       setExpenses(expenses)
       return;
     }
@@ -25,6 +29,12 @@ const RecentExpenses = () => {
     return expense.date >= date7DaysAgo && expense.date <= today;
   });
 
+  return ( isLoading ? <LoadingOverlay /> : (
+    <ExpensesOutput
+      fallbackText={"No expenses registered for the last 7 days."}
+      expenses={recentExpenses}
+      expensesPeriod={"Last 7 Days"}
+    />
   )
   );
 };
