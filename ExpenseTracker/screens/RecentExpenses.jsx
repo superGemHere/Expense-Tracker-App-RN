78ba32dd0b718092eb4c22e8e116e6895756cf97
Dzhem Rushen Syuleyman <122823771+superGemHere@users.noreply.github.com>
2024-12-29 20:31:18 +0200

    feat: connect to backend

--- ExpenseTracker/screens/RecentExpenses.jsx
+++ ExpenseTracker/screens/RecentExpenses.jsx
@@ -1,22 +1,32 @@
-import { View, Text } from 'react-native'
-import React, { useContext } from 'react'
-import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput'
-import { ExpensesContext } from '../store/context/expenses-context';
-import { getDateMinusDays } from '../utils/date';
+import { View, Text } from "react-native";
+import React, { useContext, useEffect, useState } from "react";
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
+import { ExpensesContext } from "../store/context/expenses-context";
+import { getDateMinusDays } from "../utils/date";
+import { getExpenses, } from "../utils/http";
 
 const RecentExpenses = () => {
-   const {expenses} =  useContext(ExpensesContext);
+  const { expenses, setExpenses } = useContext(ExpensesContext);
 
-   const recentExpenses = expenses.filter(expense => {
-      const today = new Date();
-      const date7DaysAgo = getDateMinusDays(today, 7);
+  useEffect(() => {
+    async function resolveExpenses() {
+      const expenses = await getExpenses();
+      setExpenses(expenses)
+      return;
+    }
 
-      return (expense.date >= date7DaysAgo) && (expense.date <= today);
-   });
+    resolveExpenses();
+  }, []);
 
-  return (
-    <ExpensesOutput fallbackText={"No expenses registered for the last 7 days."} expenses={recentExpenses} expensesPeriod={"Last 7 Days"} />
+  const recentExpenses = expenses.filter(expense => {
+    const today = new Date();
+    const date7DaysAgo = getDateMinusDays(today, 7);
+
+    return expense.date >= date7DaysAgo && expense.date <= today;
+  });
+
   )
-}
+  );
+};
 
-export default RecentExpenses
+export default RecentExpenses;
