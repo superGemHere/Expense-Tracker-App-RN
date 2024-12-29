652c9bf5a1c910e05690c602402f58de4646b12d
Dzhem Rushen Syuleyman <122823771+superGemHere@users.noreply.github.com>
2024-12-29 20:32:39 +0200

    feat: backend expense methods

--- ExpenseTracker/utils/http.js
+++ ExpenseTracker/utils/http.js
@@ -1,16 +1,39 @@
-import axios from 'axios';
+import axios from "axios";
 
 // import { BACKEND_URL } from 'react-native-dotenv'
 
-const backendURL = process.env.BACKEND_URL + "expenses.json"
+const backendURL = process.env.BACKEND_URL;
 
-export function storeExpense (expenseData) {
-   axios.post(backendURL, expenseData).then(response => {
-      console.log("Expense data stored successfully:", response.data);
-    })
-    .catch(error => {
-      console.error("Error storing expense data:", error);
-    });
-   
+export async function storeExpense(expenseData) {
+  const response = await axios.post(backendURL + "expenses.json", expenseData);
+  const id = response.data.name;
+  return id;
 }
 
+export async function getExpenses() {
+  const response = await axios.get(backendURL + "expenses.json")
+
+  
+  const expenses = [];
+
+  for (const key in response.data) {
+    const expenseObj = {
+      id: key,
+      amount: response.data[key].amount,
+      date: new Date(response.data[key].date),
+      description: response.data[key].description,
+    };
+    expenses.push(expenseObj);
+  }
+
+  return expenses;
+ 
+}
+
+export function updateExpenseRemote(id, expenseData) {
+  return axios.put(backendURL + `expenses/${id}.json`, expenseData);
+};
+
+export async function deleteExpenseRemote(id) {
+  return axios.delete(backendURL + `expenses/${id}.json`);
+};
